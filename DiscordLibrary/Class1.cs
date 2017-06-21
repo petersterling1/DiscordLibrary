9f1fa316b96fe734bd356bfeb615f0e7b364d20f
petersterling1 <psterlin@ualberta.ca>
2017-06-20 20:36:13 -0600

    Project created

---
+++ DiscordLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordLibrary
+{
+    public class Class1
+    {
+    }
+}
